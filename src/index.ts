e230fe7cfd83a3735ca4d52925676f1393691d51
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-10-08 15:46:03 +0700

    feat: add watchShallow fn

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './useLazyValue'
 export * from './useToggle'
+export * from './watchShallow'
---
+++ src/watchShallow.ts
@@ -0,0 +1,21 @@
+import {
+  WatchCallback,
+  WatchOptions,
+  WatchSource,
+  WatchStopHandle,
+} from '@vue/composition-api'
+import { MapOldSources, MapSources, watchWithFilter } from '@vueuse/core'
+import { isEqual } from '@vt7/utils'
+
+export function watchShallow<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): WatchStopHandle
+export function watchShallow<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): WatchStopHandle
+export function watchShallow<T, Immediate extends Readonly<boolean> = false>(sources: WatchSource<T>, cb: WatchCallback<T, Immediate extends true ? T | undefined : T>, options?: WatchOptions<Immediate>): WatchStopHandle
+export function watchShallow<Immediate extends Readonly<boolean> = false>(source: any, callback: any, options?: WatchOptions<Immediate>): WatchStopHandle {
+  return watchWithFilter(source, (newValue, oldValue) => {
+      if (isEqual(newValue, oldValue)) return
+
+      callback(newValue, oldValue)
+    },
+    options
+  )
+}
