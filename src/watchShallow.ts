40b3fdd27f49aa1dbab8ed78c095d1882935592e
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-10-25 15:25:28 +0700

    fix(watchShallow): fix overloads watchShallow with reactive

--- src/watchShallow.ts
+++ src/watchShallow.ts
@@ -9,6 +9,7 @@ import { isEqual } from '@vt7/utils'
 
 export function watchShallow<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): WatchStopHandle
 export function watchShallow<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): WatchStopHandle
+export function watchShallow<T extends Record<any, any>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<T, Immediate extends true ? T | undefined : T>, options?: WatchOptions<Immediate>): WatchStopHandle;
 export function watchShallow<T, Immediate extends Readonly<boolean> = false>(sources: WatchSource<T>, cb: WatchCallback<T, Immediate extends true ? T | undefined : T>, options?: WatchOptions<Immediate>): WatchStopHandle
 export function watchShallow<Immediate extends Readonly<boolean> = false>(source: any, callback: any, options?: WatchOptions<Immediate>): WatchStopHandle {
   return watchWithFilter(source, (newValue, oldValue) => {
