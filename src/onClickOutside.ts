fa2e3ccd7ff629f406d860ffa11fb8462df275e3
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-11-21 23:21:46 +0700

    feat: add watchOnce

---
+++ src/onClickOutside.ts
@@ -0,0 +1,58 @@
+import { ref } from '@vue/composition-api'
+import { MaybeElementRef, unrefElement } from './unrefElement'
+import { useEventListener } from './useEventListener'
+import { ConfigurableWindow, defaultWindow } from './_configurable'
+
+/**
+ * Listen for clicks outside of an element.
+ *
+ * @see https://vueuse.org/onClickOutside
+ * @param target
+ * @param handler
+ * @param options
+ */
+export function onClickOutside(
+  target: MaybeElementRef,
+  handler: (evt: PointerEvent) => void,
+  options: ConfigurableWindow = {}
+) {
+  const { window = defaultWindow } = options
+
+  if (!window) return
+
+  const shouldListen = ref(true)
+
+  const listener = (event: PointerEvent) => {
+    const el = unrefElement(target)
+
+    if (
+      !el ||
+      el === event.target ||
+      event.composedPath().includes(el) ||
+      !shouldListen.value
+    )
+      return
+
+    handler(event)
+  }
+
+  const cleanup = [
+    useEventListener(window, 'click', listener, {
+      passive: true,
+      capture: true,
+    }),
+    useEventListener(
+      window,
+      'pointerdown',
+      e => {
+        const el = unrefElement(target)
+        shouldListen.value = !!el && !e.composedPath().includes(el)
+      },
+      { passive: true }
+    ),
+  ]
+
+  const stop = () => cleanup.forEach(fn => fn())
+
+  return stop
+}
---
+++ src/useKeyStroke.ts
@@ -0,0 +1,96 @@
+import { MaybeRef } from '../utils/types'
+import { useEventListener } from './useEventListener'
+import { defaultWindow } from './_configurable'
+
+export type KeyPredicate = (event: KeyboardEvent) => boolean
+export type KeyFilter = null | undefined | string | string[] | KeyPredicate
+export type KeyStrokeEventName = 'keydown' | 'keypress' | 'keyup'
+export type KeyStrokeOptions = {
+  eventName?: KeyStrokeEventName
+  target?: MaybeRef<EventTarget>
+  passive?: boolean
+}
+
+const createKeyPredicate = (keyFilter: KeyFilter): KeyPredicate => {
+  if (typeof keyFilter === 'function') return keyFilter
+  else if (typeof keyFilter === 'string')
+    return (event: KeyboardEvent) => event.key === keyFilter
+  else if (Array.isArray(keyFilter))
+    return (event: KeyboardEvent) => keyFilter.includes(event.key)
+  else if (keyFilter) return () => true
+  else return () => false
+}
+
+/**
+ * Listen for keyboard keys being stroked.
+ *
+ * @see https://vueuse.org/onKeyStroke
+ * @param key
+ * @param handler
+ * @param options
+ */
+export function onKeyStroke(
+  key: KeyFilter,
+  handler: (event: KeyboardEvent) => void,
+  options: KeyStrokeOptions = {}
+) {
+  const {
+    target = defaultWindow,
+    eventName = 'keydown',
+    passive = false,
+  } = options
+  const predicate = createKeyPredicate(key)
+  const listener = (e: KeyboardEvent) => {
+    if (predicate(e)) handler(e)
+  }
+
+  return useEventListener(target, eventName, listener, passive)
+}
+
+/**
+ * Listen to the keydown event of the given key.
+ *
+ * @see https://vueuse.org/onKeyStroke
+ * @param key
+ * @param handler
+ * @param options
+ */
+export function onKeyDown(
+  key: KeyFilter,
+  handler: (event: KeyboardEvent) => void,
+  options: Omit<KeyStrokeOptions, 'eventName'> = {}
+) {
+  return onKeyStroke(key, handler, { ...options, eventName: 'keydown' })
+}
+
+/**
+ * Listen to the keypress event of the given key.
+ *
+ * @see https://vueuse.org/onKeyStroke
+ * @param key
+ * @param handler
+ * @param options
+ */
+export function onKeyPressed(
+  key: KeyFilter,
+  handler: (event: KeyboardEvent) => void,
+  options: Omit<KeyStrokeOptions, 'eventName'> = {}
+) {
+  return onKeyStroke(key, handler, { ...options, eventName: 'keypress' })
+}
+
+/**
+ * Listen to the keyup event of the given key.
+ *
+ * @see https://vueuse.org/onKeyStroke
+ * @param key
+ * @param handler
+ * @param options
+ */
+export function onKeyUp(
+  key: KeyFilter,
+  handler: (event: KeyboardEvent) => void,
+  options: Omit<KeyStrokeOptions, 'eventName'> = {}
+) {
+  return onKeyStroke(key, handler, { ...options, eventName: 'keyup' })
+}
