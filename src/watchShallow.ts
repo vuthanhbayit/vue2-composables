d35cd324201d42df9073d1b6acff90214f191588
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-11-26 23:12:49 +0700

    fix: use @vt7/vueuse

--- src/watchShallow.ts
+++ src/watchShallow.ts
@@ -5,7 +5,8 @@ import {
   WatchStopHandle,
 } from '@vue/composition-api'
 import { isEqual } from '@vt7/utils/dist/is'
-import { MapOldSources, MapSources, watchWithFilter } from '@vueuse/core'
+import { watchWithFilter } from '@vt7/vueuse/dist/shared/watchWithFilter'
+import { MapOldSources, MapSources } from '@vt7/vueuse'
 
 export function watchShallow<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): WatchStopHandle
 export function watchShallow<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): WatchStopHandle
